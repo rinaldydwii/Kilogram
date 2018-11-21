d43b83474a328d14254660ecdb910536dbdb5e68
Rinaldy Dwi Istanto <rinaldydwii@gmail.com>
2018-11-21 20:47:30 +0700

    layouting feed screen

--- src/screens/FeedScreen.js
+++ src/screens/FeedScreen.js
@@ -1,11 +1,32 @@
 import React, { Component } from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, ScrollView, Image, Dimensions } from "react-native";
 
 class FeedScreen extends Component {
     render() {
         return (
             <View style={styles.container}>
-                <Text>FeedScreen</Text>
+                <View style={styles.headerContainer}>
+                    <Text>Feed</Text>
+                </View>
+                <ScrollView>
+                    <View>
+                        <View>
+                            <Text>Username</Text>
+                        </View>
+                        <View>
+                            <Image
+                                source={{uri: 'https://source.unsplash.com/random/500x'+Math.floor(Math.random()*800) + 500 }}
+                                style={styles.feedImage}
+                            />
+                        </View>
+                        <View>
+                            <Text>Caption</Text>
+                            <Text>Comments</Text>
+                            <Text>Time ago</Text>
+                        </View>
+
+                    </View>
+                </ScrollView>
             </View>
         );
     }
@@ -15,7 +36,19 @@ export default FeedScreen;
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center'
+    },
+    headerContainer: {
+        height: 70,
+        paddingTop: 20,
+        backgroundColor: "white",
+        justifyContent: "center",
+        alignItems: "center",
+        borderColor: "#DFDFDF",
+        borderBottomWidth: 1
+    },
+    feedImage: {
+        resizeMode: "cover",
+        width: '100%',
+        height: Dimensions.get('window').width
     }
 });
