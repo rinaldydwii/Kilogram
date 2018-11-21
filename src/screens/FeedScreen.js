a9b9d702343a3170c0698915ffc8e1de4b43f3ef
Rinaldy Dwi Istanto <rinaldydwii@gmail.com>
2018-11-21 21:09:24 +0700

    adding flatlist

--- src/screens/FeedScreen.js
+++ src/screens/FeedScreen.js
@@ -1,32 +1,53 @@
 import React, { Component } from "react";
-import { View, Text, StyleSheet, ScrollView, Image, Dimensions } from "react-native";
+import { View, Text, StyleSheet, ScrollView, Image, Dimensions, FlatList } from "react-native";
 
 class FeedScreen extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            feeds: [0, 1, 2, 3, 4, 5],
+            refresh: false
+        }
+    }
+    loadNew = () => {
+        this.setState({refresh: true})
+        this.setState({
+            feeds: [6, 7, 8, 9, 10, 11],
+            refresh: false
+        })
+    }
     render() {
         return (
             <View style={styles.container}>
                 <View style={styles.headerContainer}>
                     <Text>Feed</Text>
                 </View>
-                <ScrollView>
-                    <View>
+                <FlatList 
+                    refreshing={this.state.refresh}
+                    onRefresh={this.loadNew}
+                    data={this.state.feeds}
+                    keyExtractor={(item, index) => index.toString()}
+                    renderItem={({item, index}) => (
                         <View>
-                            <Text>Username</Text>
-                        </View>
-                        <View>
-                            <Image
-                                source={{uri: 'https://source.unsplash.com/random/500x'+Math.floor(Math.random()*800) + 500 }}
-                                style={styles.feedImage}
-                            />
-                        </View>
-                        <View>
-                            <Text>Caption</Text>
-                            <Text>Comments</Text>
-                            <Text>Time ago</Text>
-                        </View>
+                            <View>
+                                <Text>Username</Text>
+                            </View>
+                            <View>
+                                <Image
+                                    source={{uri: 'https://source.unsplash.com/random/500x'+Math.floor(Math.random()*800) + 500 }}
+                                    style={styles.feedImage}
+                                />
+                            </View>
+                            <View>
+                                <Text>Caption</Text>
+                                <Text>Comments</Text>
+                                <Text>Time ago</Text>
+                            </View>
 
-                    </View>
-                </ScrollView>
+                        </View>
+                    )}
+                />
+                    
             </View>
         );
     }
