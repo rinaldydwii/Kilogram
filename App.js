859ac2586d54de21dc3fbf3da240971eef3aecc2
Rinaldy Dwi Istanto <rinaldydwii@gmail.com>
2018-11-23 10:26:36 +0800

    upgrade react navigation 3.0

--- App.js
+++ App.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import { createBottomTabNavigator } from 'react-navigation'
+import { createBottomTabNavigator, createAppContainer } from 'react-navigation'
 import FeedScreen from './src/screens/FeedScreen';
 import UploadScreen from './src/screens/UploadScreen';
 import ProfileScreen from './src/screens/ProfileScreen';
 
-const MainStack = createBottomTabNavigator(
+const MainStackNavigator = createBottomTabNavigator(
   {
     Feed: { screen: FeedScreen },
     Upload: { screen: UploadScreen },
     Profile: { screen: ProfileScreen }
   }
 )
+
+const MainStack = createAppContainer(MainStackNavigator)
 
 export default class App extends React.Component {
   render() {
