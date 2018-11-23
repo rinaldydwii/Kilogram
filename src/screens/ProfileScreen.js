8416b6fa8d3e680e22d46dddaa16163e9d173820
Rinaldy Dwi Istanto <rinaldydwii@gmail.com>
2018-11-23 19:37:28 +0800

    apply auth firebase on profile and upload screen

--- src/screens/ProfileScreen.js
+++ src/screens/ProfileScreen.js
@@ -1,11 +1,39 @@
 import React, { Component } from "react";
 import { View, Text, StyleSheet } from "react-native";
+import { f, database } from '../configs/config'
 
 class ProfileScreen extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            loggedIn: false
+        }
+    }
+    componentDidMount() {
+        f.auth().onAuthStateChanged(user => {
+            if (user) {
+                this.setState({
+                    loggedIn: true
+                })
+            } else {
+                this.setState({
+                    loggedIn: false
+                })
+            }
+        })
+    }
     render() {
         return (
             <View style={styles.container}>
-                <Text>ProfileScreen</Text>
+                { this.state.loggedIn ? 
+                    <Text>Text</Text> : 
+                    (
+                        <View>
+                            <Text>You aren't login</Text>
+                            <Text>Please login to view your profile</Text>
+                        </View>
+                    )
+                }
             </View>
         );
     }
--- src/screens/UploadScreen.js
+++ src/screens/UploadScreen.js
@@ -1,11 +1,39 @@
 import React, { Component } from "react";
 import { View, Text, StyleSheet } from "react-native";
+import { f, database } from '../configs/config'
 
 class UploadScreen extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            loggedIn: false
+        }
+    }
+    componentDidMount() {
+        f.auth().onAuthStateChanged(user => {
+            if (user) {
+                this.setState({
+                    loggedIn: true
+                })
+            } else {
+                this.setState({
+                    loggedIn: false
+                })
+            }
+        })
+    }
     render() {
         return (
             <View style={styles.container}>
-                <Text>UploadScreen</Text>
+                { this.state.loggedIn ? 
+                    <Text>Text</Text> : 
+                    (
+                        <View>
+                            <Text>You aren't login</Text>
+                            <Text>Please login to upload a photo</Text>
+                        </View>
+                    )
+                }
             </View>
         );
     }
